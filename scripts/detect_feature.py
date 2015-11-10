cf99f0e5b52c40135722ae92de330f63c6f5eb70
srli <sophia.li@students.olin.edu>
2015-11-10 14:25:08 -0500

    added arduino integration

--- scripts/detect_feature.py
+++ scripts/detect_feature.py
@@ -42,14 +42,14 @@ def main():
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
             faces = face_cascade.detectMultiScale(gray, scaleFactor=1.2, minSize=(20,20))
             for (x,y,w,h) in faces:
-                # cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),2)
+                cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),2)
                 roi_gray = gray[y:y+h, x:x+w]
                 roi_color = frame[y:y+h, x:x+w]
 
                 mouth = mouth_cascade.detectMultiScale(roi_gray, scaleFactor=1.7, minNeighbors=20, minSize=(10,10), flags=cv2.cv.CV_HAAR_SCALE_IMAGE)
 
                 for (mp,mq,mr,ms) in mouth:
-                    # cv2.rectangle(roi_color,(mp,mq),(mp+mr,mq+ms), (255,0,0),1)
+                    cv2.rectangle(roi_color,(mp,mq),(mp+mr,mq+ms), (255,0,0),1)
                     pub_face.publish(True)
             i = 0
 
--- scripts/vision_master.py
+++ scripts/vision_master.py
@@ -2,14 +2,31 @@
 
 import roslib; roslib.load_manifest('poe_vision')
 from std_msgs.msg import String, Int16, Bool
+# from sensor_msgs.msg import Image
+# from stereo_msgs.msg import DisparityImage
+from poe_kinect.msg import *
 import rospy
 import time
+import cv2
+# from cv_bridge import CvBridge, CvBridgeError
+import serial
+import time
+
+class SerialConnection:
+	def __init__(self):
+		self.addr = '/dev/ttyACM0'
+		self.baud = 9600
+		self.port = serial.Serial(self.addr, self.baud, timeout=2)
+
 
 class VisualChecker():
 	def __init__(self):
 		rospy.init_node("vision_master", anonymous=True)
 		rospy.Subscriber("/ocr_detected", String, self.ocr_callback)
 		rospy.Subscriber("/smile_detected", Bool, self.smile_callback)
+		rospy.Subscriber("/point_location", pointerpos, self.location_callback)
+		rospy.Subscriber("/detected_gestures", gestures, self.gesture_callback)
+		self.mode = 0
 
 	def ocr_callback(self, data):
 		pass
@@ -18,9 +35,37 @@ class VisualChecker():
 	def smile_callback(self, data):
 		print data.data
 
+	def location_callback(self, data):
+		xpos = data.positionx
+		if xpos != 0:
+			if -300 < xpos < -200:
+				self.mode = 2
+			elif -200 < xpos < -100:
+				self.mode = 3
+			elif -100 < xpos < 100:
+				self.mode = 4
+			elif 100 < xpos < 200:
+				self.mode = 5
+			elif 200 < xpos < 300:
+				self.mode = 1
+			# print xpos
+
+	def gesture_callback(self, data):
+		if data.wave == True:
+			print "hello there!"
+		# print data
+
 	def run(self):
+		ser = SerialConnection()
+		r = rospy.Rate(20)
+		self.prev_mode = 0
 		while not rospy.is_shutdown():
-			time.sleep(1)
+			if self.mode != self.prev_mode:
+				ser.port.write(str(self.mode))
+				print "write mode " + str(self.mode)
+				self.prev_mode = self.mode
+				# time.sleep(2)
+			r.sleep()
 
 
 if __name__ == "__main__":
