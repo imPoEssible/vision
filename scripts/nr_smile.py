7c3da440c954d120798650ab3f855c56c8a22a1a
srli <sophia.li@students.olin.edu>
2015-11-19 01:03:39 -0500

    fixed load issues

--- scripts/nr_smile.py
+++ scripts/nr_smile.py
@@ -17,21 +17,25 @@ class SmileDetector:
 
     def run(self):
         cap = cv2.VideoCapture(0)
+        i = 0
         while True:
             ret, self.frame = cap.read()
-            gray = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
-            faces = self.face_cascade.detectMultiScale(gray, scaleFactor=1.2, minSize=(20,20))
-            print len(faces)
-            for (x,y,w,h) in faces:
-                print "FACE"
-                cv2.rectangle(self.frame,(x,y),(x+w,y+h),(0,0,255),2)
-                roi_gray = gray[y:y+h, x:x+w]
-                roi_color = self.frame[y:y+h, x:x+w]
+            if i == 15:
+                gray = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
+                faces = self.face_cascade.detectMultiScale(gray, scaleFactor=1.2, minSize=(20,20))
+                print len(faces)
+                for (x,y,w,h) in faces:
+                    print "FACE"
+                    cv2.rectangle(self.frame,(x,y),(x+w,y+h),(0,0,255),2)
+                    roi_gray = gray[y:y+h, x:x+w]
+                    roi_color = self.frame[y:y+h, x:x+w]
 
-                mouth = self.mouth_cascade.detectMultiScale(roi_gray, scaleFactor=1.7, minNeighbors=20, minSize=(10,10), flags=cv2.cv.CV_HAAR_SCALE_IMAGE)
-                for (mp,mq,mr,ms) in mouth:
-                    cv2.rectangle(roi_color,(mp,mq),(mp+mr,mq+ms), (255,0,0),1)
-                    print "SMILE"
+                    mouth = self.mouth_cascade.detectMultiScale(roi_gray, scaleFactor=1.7, minNeighbors=20, minSize=(10,10), flags=cv2.cv.CV_HAAR_SCALE_IMAGE)
+                    for (mp,mq,mr,ms) in mouth:
+                        cv2.rectangle(roi_color,(mp,mq),(mp+mr,mq+ms), (255,0,0),1)
+                        print "SMILE"
+                i = 0
+            i += 1
 
             cv2.imshow("output", self.frame)
             c = cv2.waitKey(1)
