59a623cc6699797bd29e5f776d54584d30666ca1
srli <sophia.li@students.olin.edu>
2015-12-10 21:17:18 -0500

    cv integration

--- face.py
+++ face.py
@@ -6,6 +6,57 @@
 import pygame, random, math, time
 from pygame.locals import *
 from math import pi
+import cv2
+import numpy as np
+import serial
+import time
+
+class SerialConnection:
+    def __init__(self):
+        self.addr = '/dev/ttyACM0'
+        self.baud = 9600
+        self.port = serial.Serial(self.addr, self.baud, timeout=2)
+
+class SmileDetector:
+    def __init__(self):
+        self.face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml') #face xml
+        self.mouth_cascade = cv2.CascadeClassifier('haarcascade_smile.xml') #smile xml
+
+    def run(self):
+        try:
+            ser = serial.Serial('/dev/ttyACM0', 9600, timeout=2)
+        except:
+            raise Exception("Serial port not open, connect Arduino?")
+        cap = cv2.VideoCapture(0)
+        i = 0
+        while True:
+            ret, self.frame = cap.read()
+            if i == 15:
+                gray = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
+                faces = self.face_cascade.detectMultiScale(gray, scaleFactor=1.2, minSize=(20,20))
+                print len(faces)
+            if len(faces) == 0:
+                ser.write("2")
+                continue
+            for (x,y,w,h) in faces:
+                print "FACE"
+                cv2.rectangle(self.frame,(x,y),(x+w,y+h),(0,0,255),2)
+                roi_gray = gray[y:y+h, x:x+w]
+                roi_color = self.frame[y:y+h, x:x+w]
+
+                mouth = self.mouth_cascade.detectMultiScale(roi_gray, scaleFactor=1.7, minNeighbors=20, minSize=(10,10), flags=cv2.cv.CV_HAAR_SCALE_IMAGE)
+                for (mp,mq,mr,ms) in mouth:
+                    cv2.rectangle(roi_color,(mp,mq),(mp+mr,mq+ms), (255,0,0),1)
+                    print "SMILE"
+                    ser.write("1")
+                i = 0
+            i += 1
+
+            cv2.imshow("output", self.frame)
+            c = cv2.waitKey(1)
+
+        cap.release()
+        cv2.destroyAllWindows()
 
 class Model:
     def __init__(self):
@@ -57,7 +108,6 @@ class View:
 
         pygame.display.update()
 
-
 class Controller:
     """ Manipulate game state based on keyboard input """
     def __init__(self, model):
@@ -73,6 +123,9 @@ class Controller:
         x, y = event.pos
         self.model.expression = "blush"
         self.model.color = self.model.PINK
+
+def run_screen():
+    pass
 
 if __name__ == '__main__':
     pygame.init()
